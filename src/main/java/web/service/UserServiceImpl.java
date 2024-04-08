bd2e3d3eac52f49a6663d5409515b72826cac2c4
AsyaSkoraya <153989033+AsyaSkoraya@users.noreply.github.com>
2024-04-08 23:10:45 +0500

    2/1/3

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+import web.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private final UserRepository userRepository;
+
+
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return userRepository.getAllUsers();
+    }
+
+    @Override
+    public User readUser(Long id) {
+        return userRepository.readUser(id);
+    }
+
+    @Override
+    public void createOrUpdateUser(User user) {
+        if (user.getId()!=null){
+            userRepository.updateUser(user);
+        }else {
+            userRepository.createUser(user);
+        }
+
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        try {
+            userRepository.deleteUser(id);
+        } catch (NullPointerException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user){
+        userRepository.updateUser(user);
+
+    }
+}
